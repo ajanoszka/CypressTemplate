34e26bebc1b000cb32bb7807588c99dc858b18f4
Alan <alan@MacBook-Pro-Alan.local>
2020-12-15 14:05:51 +0100

    Test fixed

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -3,10 +3,6 @@ describe('TEST OF www.phptravels.net SITE', () => {
     before('CHECK ENV VARIABLES', () => {
       expect(Cypress.env()).to.include.keys('Url', 'Username', 'Password')
     })
-
-    it('CHECK ENV VARIABLES', () => {
-      expect(Cypress.env()).to.include.keys('Url', 'Username', 'Password')
-    })
   
     it('VISIT MAIN PAGE', () => {
         cy.visit(Cypress.env('Url'))
